0eeebfb6a9fa915e1213d319b58ad2322c66042b
Michael Clarke <bspride11@gmail.com>
2017-04-10 00:59:36 -0400

    Add firebase config to src control

---
+++ src/app/secrets/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB9T49Ymab0TyaFhx3LbYujFYjn-1AFKd8",
+  authDomain: "playoffpredictor-6bc11.firebaseapp.com",
+  databaseURL: "https://playoffpredictor-6bc11.firebaseio.com",
+  projectId: "playoffpredictor-6bc11",
+  storageBucket: "playoffpredictor-6bc11.appspot.com",
+  messagingSenderId: "464554240422"
+};
