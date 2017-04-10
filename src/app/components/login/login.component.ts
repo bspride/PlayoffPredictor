456daf0d3907db26b22ecfeb0065f8fbf269969a
Michael Clarke <bspride11@gmail.com>
2017-04-09 23:44:29 -0400

    Add login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.html'
+})
+export class LoginComponent {
+  constructor(public af: AngularFire) {}
+
+  login() {
+    this.af.auth.login({
+      email: 'email',
+      password: 'pass'
+    });
+  }
+
+  googleLogin() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Redirect
+    });
+  }
+}
